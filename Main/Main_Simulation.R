07dcd75ebf09511579671703a07fb527bf6408f5
dtsoucas <dtsoucas@gmail.com>
2017-11-29 21:54:30 -0500

    Update Main_Simulation.R

--- Main/Main_Simulation.R
+++ Main/Main_Simulation.R
@@ -13,15 +13,14 @@ GeneList             = 1                                                # parame
 Gamma                = 0.99                                             # parameter for clustering
 diff.cutoff          = 1                                                # MAST analysis, filter genes that don't have high log2_foldchange to reduce gene num
 lr.p_value_cutoff    = 1e-5                                             # MAST analysis, pvalue cutoff to identify differentially expressed genes
-CountsForNormalized  = 100000											                      # if normalizing- by default not used
-Rfundir              = "/Users/Daphne/Documents/Yuan/GiniClust2_V1/
-														Rfunction/"		                              # where GiniClust2 R functions are stored
+CountsForNormalized  = 100000						# if normalizing- by default not used
+Rfundir              = "/Users/Daphne/Documents/Yuan/GiniClust2_V1/Rfunction/"	
+									# where GiniClust2 R functions are stored
 
 #dataset-specific parameters:
 eps                  = 0.5                                              # parameter for DBSCAN
 MinPts               = 3                                                # parameter for DBSCAN
-mycols               = c("grey50","greenyellow","red","blue","black",
-															"orange")             							      # color setting for tSNE plot
+mycols               = c("grey50","greenyellow","red","blue","black","orange")             							      # color setting for tSNE plot
 perplexity_G         = 1e-5 											  							      # parameter for Gini tSNE
 perplexity_F         = 30 												  							      # parameter for Fano tSNE
 max_iter_G           = 10000 											  							      # parameter for Gini tSNE
@@ -31,9 +30,8 @@ gap_statistic        = TRUE											  							  			# whether the gap statistic 
 K.max                = 10 												  							  		# if using the gap statistic, highest k that should be considered
 automatic_eps        = TRUE 											  							  		# whether to determine eps using KNN
 automatic_minpts     = TRUE												  							  		# whether to determine MinPts based on the size of the data set
-workdir              = "/Users/Daphne/Documents/Yuan/GiniClust2_V1/
-													Proj/Simulation/" 	  							  				# where you put the data and results
-exprimentID          = "simu"                              				      # experiment or data set ID
+workdir              = "/Users/Daphne/Documents/Yuan/GiniClust2_V1/Proj/Simulation/" 	  							  				# where you put the data and results
+exprimentID          = "simu"                              		# experiment or data set ID
 
 
 setwd(workdir)
