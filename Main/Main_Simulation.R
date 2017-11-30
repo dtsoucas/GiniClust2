1a766fc4e191d57ac553202f504c89ea6569d9bf
dtsoucas <dtsoucas@gmail.com>
2017-11-30 16:38:53 -0500

    Update Main_Simulation.R

--- Main/Main_Simulation.R
+++ Main/Main_Simulation.R
@@ -35,6 +35,8 @@ exprimentID          = "simu"                              		# experiment or dat
 
 
 setwd(workdir)
+dir.create(file.path(workdir, "results"), showWarnings = FALSE) #folder to save results
+dir.create(file.path(workdir, "figures"), showWarnings = FALSE) #folder to save figures
 #load packages and functions
 source(paste(Rfundir,"GiniClust2_packages.R",sep=""))
 source(paste(Rfundir,"GiniClust2_functions.R",sep=""))
